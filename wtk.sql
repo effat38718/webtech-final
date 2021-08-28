a372113066440676a9ce2e9ce5de690886423091
Effat Jahan <75140801+effat38718@users.noreply.github.com>
2021-08-28 13:10:25 +0600

    database migration script

--- wtk.sql
+++ wtk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 28, 2021 at 08:55 AM
+-- Generation Time: Aug 28, 2021 at 09:09 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.4.21
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `wtk`
 --
+CREATE DATABASE IF NOT EXISTS `wtk` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `wtk`;
 
 -- --------------------------------------------------------
 
